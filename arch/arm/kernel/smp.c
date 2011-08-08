6ef3937c2c749e41a59f2d9d4b8700828cd4c47e
Vincent Guittot <vincent.guittot@linaro.org>
2011-08-08 13:21:59 +0100

    ARM: 7011/1: Add ARM cpu topology definition
    The affinity between ARM processors is defined in the MPIDR register.
    We can identify which processors are in the same cluster,
    and which ones have performance interdependency. We can define the
    cpu topology of ARM platform, that is then used by sched_mc and sched_smt.
    
    The default state of sched_mc and sched_smt config is disable.
    When enabled, the behavior of the scheduler can be modified with
    sched_mc_power_savings and sched_smt_power_savings sysfs interfaces.
    
    Changes since v4 :
    *  Remove unnecessary parentheses and blank lines
    
    Changes since v3 :
    * Update the format of printk message
    * Remove blank line
    
    Changes since v2 :
    * Update the commit message and some comments
    
    Changes since v1 :
    * Update the commit message
    * Add read_cpuid_mpidr in arch/arm/include/asm/cputype.h
    * Modify header of arch/arm/kernel/topology.c
    * Modify tests and manipulation of MPIDR's bitfields
    * Modify the place and dependancy of the config
    * Modify Noop functions
    
    Signed-off-by: Vincent Guittot <vincent.guittot@linaro.org>
    Reviewed-by: Amit Kucheria <amit.kucheria@linaro.org>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>
    
    Conflicts:
    
    	arch/arm/include/asm/topology.h
    	arch/arm/kernel/smp.c
    	arch/arm/kernel/topology.c

--- arch/arm/kernel/smp.c
+++ arch/arm/kernel/smp.c
@@ -32,6 +32,7 @@
 #include <asm/cputype.h>
 #include <asm/exception.h>
 #include <asm/idmap.h>
+
 #include <asm/topology.h>
 #include <asm/mmu_context.h>
 #include <asm/pgtable.h>
@@ -239,8 +240,10 @@ static void __cpuinit smp_store_cpu_info(unsigned int cpuid)
 
 	cpu_info->loops_per_jiffy = loops_per_jiffy;
 
+
 	store_cpu_topology(cpuid);
 }
+
 
 /*
  * This is the secondary CPU boot entry.  We're using this CPUs
