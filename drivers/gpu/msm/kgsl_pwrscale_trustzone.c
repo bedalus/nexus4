1768c81965958be5192e61b44f35fe6d2cd24ee9
Francisco Franco <franciscofranco.1990@gmail.com>
2013-06-28 16:35:12 +0100

    KGSL: Simple Governor: so I kinda butchered in a good way of course the original code from faux, but the simplicity is still here. Added an history so that we can track the average do that the frequency scaling is much more proporcional to the actual GPU load rather than having is ramp up every now and then due to spurious load spike. Also remove two useless ifs.
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- drivers/gpu/msm/kgsl_pwrscale_trustzone.c
+++ drivers/gpu/msm/kgsl_pwrscale_trustzone.c
@@ -148,26 +148,34 @@ static void tz_wake(struct kgsl_device *device, struct kgsl_pwrscale *pwrscale)
 }
 
 #ifdef CONFIG_MSM_KGSL_SIMPLE_GOV
-static int default_laziness = 5;
-module_param_named(simple_laziness, default_laziness, int, 0664);
+#define HISTORY_SIZE 10
 
-static int ramp_up_threshold = 6000;
+static int ramp_up_threshold = 5500;
 module_param_named(simple_ramp_threshold, ramp_up_threshold, int, 0664);
 
-static int laziness;
+static unsigned int history[HISTORY_SIZE] = {0};
+static unsigned int counter = 0;
 
 static int simple_governor(struct kgsl_device *device, int idle_stat)
 {
 	struct kgsl_pwrctrl *pwr = &device->pwrctrl;
+	int i;
+	unsigned int total = 0;
+
+	history[counter] = idle_stat;
+
+	for (i = 0; i < HISTORY_SIZE; i++)
+		total += history[i];
+
+	total = total/HISTORY_SIZE;
+
+	if (++counter == 10)
+		counter = 0;
 
 	/* it's currently busy */
-	if (idle_stat < ramp_up_threshold) 
+	if (total < ramp_up_threshold) 
 	{
-		if (pwr->active_pwrlevel == 0)
-			/* already maxed, so do nothing */
-			return 0; 
-		
-		else if ((pwr->active_pwrlevel > 0) &&
+		if ((pwr->active_pwrlevel > 0) &&
 			(pwr->active_pwrlevel <= (pwr->num_pwrlevels - 1)))
 			/* bump up to next pwrlevel */
 			return -1; 
@@ -177,20 +185,7 @@ static int simple_governor(struct kgsl_device *device, int idle_stat)
 	{
 		if ((pwr->active_pwrlevel >= 0) &&
 			(pwr->active_pwrlevel < (pwr->num_pwrlevels - 1)))
-			if (likely(--laziness > 0)) 
-			{
-				/* don't change anything yet hold off for a while */
-				return 0;
-			} 
-			else 
-			{
-				/* above min, lower it */	
-				laziness = default_laziness;
-				return 1; 	
-			}
-		else if (pwr->active_pwrlevel == (pwr->num_pwrlevels - 1))
-			/* already @ min, so do nothing */
-			return 0; 
+			return 1; 	 
 	}
 
 	return 0;
