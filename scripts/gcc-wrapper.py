06d94f54953778eab198e0fb3c8978e0a6cae301
hellsgod <hellsgod@gmx.ch>
2013-07-26 05:09:26 +0200

    gcc-wrapper.py: Fix derp

--- scripts/gcc-wrapper.py
+++ scripts/gcc-wrapper.py
@@ -43,7 +43,7 @@ allowed_warnings = set([
     "alignment.c:327",
     "mmu.c:602",
     "return_address.c:62",
-    "mipi_lgit:600",
+    "mipi_lgit.c:600",
  ])
 
 # Capture the name of the object file, can find it.
