250667c869cefd215a56f40e9d7357151e2b6381
David Savage <bedalus@gmail.com>
2013-12-07 21:36:34 +0000

    delete duplicate definition

--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -5292,22 +5292,7 @@ out:
 }
 #endif /* CONFIG_NET_DMA */
 
-static void tcp_send_challenge_ack(struct sock *sk)
-{
-	/* unprotected vars, we dont care of overwrites */
-	static u32 challenge_timestamp;
-	static unsigned int challenge_count;
-	u32 now = jiffies / HZ;
 
-	if (now != challenge_timestamp) {
-		challenge_timestamp = now;
-		challenge_count = 0;
-	}
-	if (++challenge_count <= sysctl_tcp_challenge_ack_limit) {
-		NET_INC_STATS_BH(sock_net(sk), LINUX_MIB_TCPCHALLENGEACK);
-		tcp_send_ack(sk);
-	}
-}
 
 /* Does PAWS and seqno based validation of an incoming segment, flags will
  * play significant role here.
