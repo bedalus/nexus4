377981f58137999d13ee0d5a956bc41b3bd353ea
David Savage <bedalus@gmail.com>
2013-12-12 05:45:08 +0000

    also build cm version

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@
     export SUBARCH=arm
     export CROSS_COMPILE=arm-unknown-linux-gnueabi-
 
+git apply -R cm.patch
 make -j7
 
 # copy zImage
@@ -12,4 +13,19 @@ ls -l ../zip/kernel/zImage
 cd ../zip
 zip -r -9 kernel.zip * > /dev/null
 mv kernel.zip ../
+
+echo "Press enter to build cm kernel"
+read enterkey
+cd ../nexus4
+git apply cm.patch
+make -j7
+
+# copy zImage
+cp -f arch/arm/boot/zImage ../zip/kernel/
+ls -l ../zip/kernel/zImage
+cd ../zip
+zip -r -9 kernel_cm.zip * > /dev/null
+mv kernel_cm.zip ../
+cd ../nexus4
+git apply -R cm.patch
 
