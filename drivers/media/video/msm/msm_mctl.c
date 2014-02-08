d82dfb6f4908bbb2e7890f5e997d5cd23cc73013
David Savage <bedalus@gmail.com>
2014-02-08 16:48:30 +0000

    camera: catch possible NULL pointer dereference

--- drivers/media/video/msm/msm_mctl.c
+++ drivers/media/video/msm/msm_mctl.c
@@ -431,8 +431,12 @@ static int msm_mctl_cmd(struct msm_cam_media_controller *p_mctl,
 		/* ISP config*/
 		D("%s:%d: go to default. Calling msm_isp_config\n",
 			__func__, __LINE__);
-		if ((p_mctl) && (p_mctl->isp_sdev) && (p_mctl->isp_sdev->isp_config))
+		if ((p_mctl) && (p_mctl->isp_sdev) && (p_mctl->isp_sdev->isp_config)) {
 			rc = p_mctl->isp_sdev->isp_config(p_mctl, cmd, arg);
+		} else {
+			rc =  -EINVAL;
+			pr_err("%s: media controller is null\n", __func__);
+		}
 		break;
 	}
 	D("%s: !!! cmd = %d, rc = %d\n",
