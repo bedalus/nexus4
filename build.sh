a2ffdaf0e538f7c711e7528d88040efebd9a71b5
David Savage <bedalus@gmail.com>
2014-02-23 10:14:21 +0000

    build.sh: build AOSP first

--- build.sh
+++ build.sh
@@ -4,20 +4,6 @@
     export SUBARCH=arm
     export CROSS_COMPILE=arm-cortex_a15-linux-gnueabihf-
 
-echo "Building cm kernel"
-git apply cm.patch
-make -j7
-
-# copy zImage
-cp -f arch/arm/boot/zImage ../zip/kernel/
-ls -l ../zip/kernel/zImage
-cd ../zip
-zip -r -9 moob_vNcm.zip * > /dev/null
-mv moob_vNcm.zip ../
-
-echo "Press enter to build AOSP kernel"
-read enterkey
-cd ../nexus4
 git apply -R cm.patch
 make -j7
 
@@ -27,6 +13,19 @@ ls -l ../zip/kernel/zImage
 cd ../zip
 zip -r -9 moob_vN.zip * > /dev/null
 mv moob_vN.zip ../
+
+echo "Press enter to build cm kernel"
+read enterkey
 cd ../nexus4
 git apply cm.patch
+make -j7
+
+# copy zImage
+cp -f arch/arm/boot/zImage ../zip/kernel/
+ls -l ../zip/kernel/zImage
+cd ../zip
+zip -r -9 moob_vNcm.zip * > /dev/null
+mv moob_vNcm.zip ../
+cd ../nexus4
+git apply -R cm.patch
 
