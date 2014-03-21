80c1dcc41e66482b043b25d482d494fad64502c1
David Savage <bedalus@gmail.com>
2014-03-21 04:22:06 +0000

    initial build setup

---
+++ build.sh
@@ -0,0 +1,31 @@
+    echo -e "Making mako zImage\n"
+    export PATH=$PATH:/opt/toolchain3/bin/
+    export ARCH=arm
+    export SUBARCH=arm
+    export CROSS_COMPILE=arm-eabi-
+
+git apply -R cm.patch
+make -j7
+
+# copy zImage
+cp -f arch/arm/boot/zImage ../zip/kernel/
+ls -l ../zip/kernel/zImage
+cd ../zip
+zip -r -9 moob_vN.zip * > /dev/null
+mv moob_vN.zip ../
+
+echo "Press enter to build cm kernel"
+read enterkey
+cd ../nexus4
+git apply cm.patch
+make -j7
+
+# copy zImage
+cp -f arch/arm/boot/zImage ../zip/kernel/
+ls -l ../zip/kernel/zImage
+cd ../zip
+zip -r -9 moob_vNcm.zip * > /dev/null
+mv moob_vNcm.zip ../
+cd ../nexus4
+git apply -R cm.patch
+
